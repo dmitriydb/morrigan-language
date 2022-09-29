e21a18e7fffb4a0443befe3bf0677dc42466f4f2
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-09-29 09:51:49 +0500

    Disable spring cloud config loading in test profile

---
+++ service-registry/src/test/java/ru/shanalotte/serviceregistry/DisabledSpringCloudConfigTest.java
@@ -0,0 +1,26 @@
+package ru.shanalotte.serviceregistry;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource("classpath:fake-cloud.properties")
+public class DisabledSpringCloudConfigTest {
+
+  @Value("${session.timeout.ms}")
+  private int sessionTimeout;
+
+  @Value("${test}")
+  private String test;
+
+  @Test
+  public void shouldWork() {
+      assertThat(test).isEqualTo("abc");
+      assertThat(sessionTimeout).isEqualTo(1234);
+  }
+}
